679160eb32a8564f523cc16e0867e89096efe39b
anaghaintern <anagha.aithal@cydratech.com>
2026-06-18 13:18:15 +0530

    Add comments for implementing filters in getVisibleTickets

--- script.js
+++ script.js
@@ -69,6 +69,12 @@ function renderTickets() {
 
 function getVisibleTickets() {
   // Intern 2: apply search, status filter, and priority filter here.
+
+
+  drydghf 
+
+  mchcc
+  jvgj
   return tickets;
 }
 
